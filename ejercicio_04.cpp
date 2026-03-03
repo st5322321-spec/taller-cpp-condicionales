508ae27c7869eac36fe02bcdfcce4cb27c222245
st5322321-spec <st5322321@gmail.com>
2026-03-03 05:26:00 -0500

    Update ejercicio_04.cpp

--- ejercicio_04.cpp
+++ ejercicio_04.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <cmath>    // Para fmod()
+
+int main() {
+    double num1, num2;
+    char operador;
+
+    std::cout << "Ingrese el primer numero: ";
+    std::cin >> num1;
+
+    std::cout << "Ingrese el operador (+, -, *, /, %): ";
+    std::cin >> operador;
+
+    std::cout << "Ingrese el segundo numero: ";
+    std::cin >> num2;
+
+    switch (operador) {
+        case '+':
+            std::cout << "Resultado: " << num1 + num2 << "\n";
+            break;
+
+        case '-':
+            std::cout << "Resultado: " << num1 - num2 << "\n";
+            break;
+
+        case '*':
+            std::cout << "Resultado: " << num1 * num2 << "\n";
+            break;
+
+        case '/':
+            if (num2 == 0) {
+                std::cout << "Error: division por cero." << "\n";
+            } else {
+                std::cout << "Resultado: " << num1 / num2 << "\n";
+            }
+            break;
+
+        case '%':
+            if (num2 == 0) {
+                std::cout << "Error: modulo por cero." << "\n";
+            } else {
+                std::cout << "Resultado: " << fmod(num1, num2) << "\n";
+            }
+            break;
+
+        default:
+            std::cout << "Error: operador no valido." << "\n";
+    }
+
+    return 0;
+}
 
