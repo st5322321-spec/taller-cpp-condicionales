e47a323e715ff635bc26e5a26a634ef25fb505dd
st5322321-spec <st5322321@gmail.com>
2026-03-03 05:38:58 -0500

    Update ejercicio_06.cpp

--- ejercicio_06.cpp
+++ ejercicio_06.cpp
@@ -1,1 +1,63 @@
+#include <iostream>
 
+int main() {
+    int a, b;
+
+    std::cout << "Ingrese el valor de a: ";
+    std::cin >> a;
+    std::cout << "Ingrese el valor de b: ";
+    std::cin >> b;
+
+    
+    {
+        std::cout << "\nMetodo (a): Variable temporal\n";
+        int x = a;
+        int y = b;
+
+        std::cout << "Antes: x = " << x << ", y = " << y << "\n";
+
+        int temp = x;
+        x = y;
+        y = temp;
+
+        std::cout << "Despues: x = " << x << ", y = " << y << "\n";
+    }
+
+    
+    {
+        std::cout << "\nMetodo (b): Operaciones aritmeticas\n";
+        int x = a;
+        int y = b;
+
+        std::cout << "Antes: x = " << x << ", y = " << y << "\n";
+
+        x += y;   // x = x + y
+        std::cout << "Paso 1 (x += y): x = " << x << ", y = " << y << "\n";
+
+        y = x - y;
+        std::cout << "Paso 2 (y = x - y): x = " << x << ", y = " << y << "\n";
+
+        x -= y;
+        std::cout << "Paso 3 (x -= y): x = " << x << ", y = " << y << "\n";
+    }
+
+    
+    {
+        std::cout << "\nMetodo (c): XOR\n";
+        int x = a;
+        int y = b;
+
+        std::cout << "Antes: x = " << x << ", y = " << y << "\n";
+
+        x ^= y;
+        std::cout << "Paso 1 (x ^= y): x = " << x << ", y = " << y << "\n";
+
+        y ^= x;
+        std::cout << "Paso 2 (y ^= x): x = " << x << ", y = " << y << "\n";
+
+        x ^= y;
+        std::cout << "Paso 3 (x ^= y): x = " << x << ", y = " << y << "\n";
+    }
+
+    return 0;
+}
